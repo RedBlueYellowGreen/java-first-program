c59ea92e869c0de712bc0b22b7a2d16fd718dc64
RedBlueYellowGreen <tobias.schroeter6712@gmail.com>
2022-12-01 17:57:12 +0100

    Finding the Best Loan Rates 1

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -23,6 +23,7 @@ public class Utilities {
     }
 
     public static float getFloatValue(String in) {
+
         float out = Float.MIN_VALUE;
         try {
             out = Float.parseFloat(in);
