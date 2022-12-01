c09d199cc1d82efdcd627a9650c7952778f717f7
RedBlueYellowGreen <tobias.schroeter6712@gmail.com>
2022-12-01 15:56:27 +0100

    Finding the Best Loan Rates 1

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -2,13 +2,13 @@ package com.h2;
 
 public class Utilities {
 
-    public static long getLongValue(String in){
-       long out = Long.MIN_VALUE;
-       try{
-           out = Long.parseLong(in);
-       } catch (NumberFormatException e) {
-           throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
-       }
+    public static long getLongValue(String in) {
+        long out = Long.MIN_VALUE;
+        try {
+            out = Long.parseLong(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
+        }
         return out;
     }
 
